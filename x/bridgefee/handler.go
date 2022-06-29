d519b1a9b0bf56328603ba3d0bdbfe8b86fa7492
jpine3528 <jpine3528@gmail.com>
2022-06-29 21:11:51 +0800

    build module and keeper infra for bridgefee

---
+++ x/bridgefee/handler.go
@@ -0,0 +1,138 @@
+package bridgefee
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/pokt-network/pocket-core/crypto"
+
+	sdk "github.com/pokt-network/pocket-core/types"
+	"github.com/pokt-network/pocket-core/x/bridgefee/keeper"
+	"github.com/pokt-network/pocket-core/x/bridgefee/types"
+)
+
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	return func(ctx sdk.Ctx, msg sdk.Msg, _ crypto.PublicKey) sdk.Result {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+		// convert to value for switch consistency
+		if reflect.ValueOf(msg).Kind() == reflect.Ptr {
+			msg = reflect.Indirect(reflect.ValueOf(msg)).Interface().(sdk.Msg)
+		}
+
+		switch msg := msg.(type) {
+		case types.MsgAddAllowedActor:
+			return handleMsgAddAllowedActor(ctx, msg, k)
+		case types.MsgRemoveAllowedActor:
+			return handleMsgRemoveAllowedActor(ctx, msg, k)
+		case types.MsgSetTokenInfo:
+			return handleMsgSetTokenInfo(ctx, msg, k)
+		case types.MsgSetTokenTargetInfos:
+			return handleMsgSetTokenTargetInfos(ctx, msg, k)
+		case types.MsgSetGlobalTargetInfos:
+			return handleMsgSetGlobalTargetInfos(ctx, msg, k)
+		default:
+			errMsg := fmt.Sprintf("unrecognized bridgefee message type: %T", msg)
+			return sdk.ErrUnknownRequest(errMsg).Result()
+		}
+	}
+}
+
+func handleMsgAddAllowedActor(ctx sdk.Ctx, msg types.MsgAddAllowedActor, k keeper.Keeper) sdk.Result {
+	moduleOwner := k.GetParams(ctx).Owner
+	if msg.FromAddress.String() != moduleOwner {
+		return types.ErrNotEnoughPermission(k.Codespace()).Result()
+	}
+
+	err := k.AddAllowedActor(ctx)
+	if err != nil {
+		return err.Result()
+	}
+
+	// ctx.EventManager().EmitEvents(sdk.Events{
+	// 	sdk.NewEvent(
+	// 		types.EventTypeSetSigner,
+	// 		sdk.NewAttribute(types.AttributeKeySigner, msg.Signer),
+	// 	)},
+	// )
+	return sdk.Result{Events: ctx.EventManager().Events()}
+}
+
+func handleMsgRemoveAllowedActor(ctx sdk.Ctx, msg types.MsgRemoveAllowedActor, k keeper.Keeper) sdk.Result {
+	moduleOwner := k.GetParams(ctx).Owner
+	if msg.FromAddress.String() != moduleOwner {
+		return types.ErrNotEnoughPermission(k.Codespace()).Result()
+	}
+
+	err := k.RemoveAllowedActor(ctx)
+	if err != nil {
+		return err.Result()
+	}
+
+	// ctx.EventManager().EmitEvents(sdk.Events{
+	// 	sdk.NewEvent(
+	// 		types.EventTypeSetSigner,
+	// 		sdk.NewAttribute(types.AttributeKeySigner, msg.Signer),
+	// 	)},
+	// )
+	return sdk.Result{Events: ctx.EventManager().Events()}
+}
+
+func handleMsgSetTokenInfo(ctx sdk.Ctx, msg types.MsgSetTokenInfo, k keeper.Keeper) sdk.Result {
+	moduleOwner := k.GetParams(ctx).Owner
+	if msg.FromAddress.String() != moduleOwner {
+		return types.ErrNotEnoughPermission(k.Codespace()).Result()
+	}
+
+	err := k.SetTokenInfo(ctx)
+	if err != nil {
+		return err.Result()
+	}
+
+	// ctx.EventManager().EmitEvents(sdk.Events{
+	// 	sdk.NewEvent(
+	// 		types.EventTypeSetSigner,
+	// 		sdk.NewAttribute(types.AttributeKeySigner, msg.Signer),
+	// 	)},
+	// )
+	return sdk.Result{Events: ctx.EventManager().Events()}
+}
+
+func handleMsgSetTokenTargetInfos(ctx sdk.Ctx, msg types.MsgSetTokenTargetInfos, k keeper.Keeper) sdk.Result {
+	moduleOwner := k.GetParams(ctx).Owner
+	if msg.FromAddress.String() != moduleOwner {
+		return types.ErrNotEnoughPermission(k.Codespace()).Result()
+	}
+
+	err := k.SetTokenTargetInfos(ctx)
+	if err != nil {
+		return err.Result()
+	}
+
+	// ctx.EventManager().EmitEvents(sdk.Events{
+	// 	sdk.NewEvent(
+	// 		types.EventTypeSetSigner,
+	// 		sdk.NewAttribute(types.AttributeKeySigner, msg.Signer),
+	// 	)},
+	// )
+	return sdk.Result{Events: ctx.EventManager().Events()}
+}
+
+func handleMsgSetGlobalTargetInfos(ctx sdk.Ctx, msg types.MsgSetGlobalTargetInfos, k keeper.Keeper) sdk.Result {
+	moduleOwner := k.GetParams(ctx).Owner
+	if msg.FromAddress.String() != moduleOwner {
+		return types.ErrNotEnoughPermission(k.Codespace()).Result()
+	}
+
+	err := k.SetGlobalTargetInfos(ctx)
+	if err != nil {
+		return err.Result()
+	}
+
+	// ctx.EventManager().EmitEvents(sdk.Events{
+	// 	sdk.NewEvent(
+	// 		types.EventTypeSetSigner,
+	// 		sdk.NewAttribute(types.AttributeKeySigner, msg.Signer),
+	// 	)},
+	// )
+	return sdk.Result{Events: ctx.EventManager().Events()}
+}
---
+++ x/bridgefee/keeper/admin.go
@@ -0,0 +1,25 @@
+package keeper
+
+import (
+	sdk "github.com/pokt-network/pocket-core/types"
+)
+
+func (k Keeper) AddAllowedActor(ctx sdk.Ctx) sdk.Error {
+	return nil
+}
+
+func (k Keeper) RemoveAllowedActor(ctx sdk.Ctx) sdk.Error {
+	return nil
+}
+
+func (k Keeper) SetTokenInfo(ctx sdk.Ctx) sdk.Error {
+	return nil
+}
+
+func (k Keeper) SetTokenTargetInfos(ctx sdk.Ctx) sdk.Error {
+	return nil
+}
+
+func (k Keeper) SetGlobalTargetInfos(ctx sdk.Ctx) sdk.Error {
+	return nil
+}
